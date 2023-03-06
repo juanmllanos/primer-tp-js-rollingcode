eba80519316c35fe90864bbf402eb10a130de74f
juanmllanos <juan_m@hotmail.com.ar>
2023-03-06 01:57:16 -0300

    solucion con switch multiples casos

--- js/ejercicio-10.js
+++ js/ejercicio-10.js
@@ -1,21 +1,12 @@
 const numero = Number(prompt('Escribe un numero'));
-let esDivisible = false;
 switch (0) {
 	case numero % 2:
-		esDivisible = true;
-		break;
 	case numero % 3:
-		esDivisible = true;
-		break;
 	case numero % 5:
-		esDivisible = true;
-		break;
 	case numero % 7:
-		esDivisible = true;
+		console.log(`Es numero ${numero} es divisible por al menos uno de los siguientes numeros: 2, 3, 5 o 7.`);
 		break;
-};
-if (esDivisible) {
-	console.log(`Es numero ${numero} es divisible por al menos uno de los siguientes numeros: 2, 3, 5 o 7.`);
-} else {
-	console.log(`Es numero ${numero} no es divisible por al menos uno de los siguientes numeros: 2, 3, 5 o 7.`);
+	default:
+		console.log(`Es numero ${numero} no es divisible por ninguno de los siguientes numeros: 2, 3, 5 o 7.`);
+		break;
 };
