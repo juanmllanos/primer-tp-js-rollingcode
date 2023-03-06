8f7e5742197b1527b5e33e84a24a9e1f8ccf7b6b
juanmllanos <juan_m@hotmail.com.ar>
2023-03-05 22:29:07 -0300

    ejercicios 5 y 6 terminados

---
+++ js/ejercicio-5.js
@@ -0,0 +1,3 @@
+const numeroUno = parseInt(prompt('Ingrese un numero :'));
+const numeroDos = parseInt(prompt('Ingrese un numero :'));
+document.write(`La suma de los dos numeros es ${numeroUno + numeroDos}`);
