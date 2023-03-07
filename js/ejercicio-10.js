050f89ce897271473f327650a834f5e9d34ced2e
juanmllanos <juan_m@hotmail.com.ar>
2023-03-07 15:07:59 -0300

    ejercicios terminados, elimine carpeta css

--- js/ejercicio-10.js
+++ js/ejercicio-10.js
@@ -4,9 +4,9 @@ switch (0) {
 	case numero % 3:
 	case numero % 5:
 	case numero % 7:
-		console.log(`Es numero ${numero} es divisible por al menos uno de los siguientes numeros: 2, 3, 5 o 7.`);
+		document.write(`Es numero ${numero} es divisible por al menos uno de los siguientes numeros: 2, 3, 5 o 7.`);
 		break;
 	default:
-		console.log(`Es numero ${numero} no es divisible por ninguno de los siguientes numeros: 2, 3, 5 o 7.`);
+		document.write(`Es numero ${numero} no es divisible por ninguno de los siguientes numeros: 2, 3, 5 o 7.`);
 		break;
 };
